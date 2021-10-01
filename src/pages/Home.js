1a46d168f17f65fce1f0a4695345c1d754f5aa20
Ayush Kumar <kumarayush7767@gmail.com>
2021-10-01 20:27:13 +0530

    HeroSection added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
 
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 export default function Home() {
     return (
         <div>
-            <h1>This is home page</h1>
+            <HeroSection/>
         </div>
     )
 };
