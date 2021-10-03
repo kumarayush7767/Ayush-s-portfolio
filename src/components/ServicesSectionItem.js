d56ad526b74cb1cb012b582f9486012c7aea73e2
Ayush Kumar <kumarayush7767@gmail.com>
2021-10-03 13:20:48 +0530

    i have added Services component

---
+++ src/components/ServicesSectionItem.js
@@ -0,0 +1,39 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { MdDesktopMac } from 'react-icons/md';
+import styled from 'styled-components';
+import PText from './PText';
+
+const ItemStyles = styled.div`
+  text-align: center;
+  .servicesItem__icon {
+    svg {
+      width: 3rem;
+    }
+  }
+  .servicesItem__title {
+    font-size: 2.5rem;
+    font-family: "Montserrat SemiBold";
+  }
+  .para {
+    margin-top: 2rem;
+  }
+`;
+
+const ServicesSectionItem = ({
+  icon = <MdDesktopMac />,
+  title = 'Web Design',
+  desc = "Lorem ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's ok",
+}) => {
+  return (
+    <ItemStyles>
+      <div className="servicesItem__icon">{icon}</div>
+      <div className="servicesItem__title">{title}</div>
+      <PText>{desc}</PText>
+    </ItemStyles>
+  );
+};
+
+export default ServicesSectionItem;
